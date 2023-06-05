84eb72c67867f9347f84db885fe0919e4326b68f
OlaniyiHope <olaniyihoppee@gmail.com>
2023-06-05 06:47:17 +0100

    all

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ mongoose.connection.on("disconnected", () => {
 app.use(cookieParser());
 app.use(express.json());
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://newsflashapi.vercel');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
